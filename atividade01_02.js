4cae32bf5ac70431619de20d311f48ec7b4a9d99
Giovanni <giovanni.ggoncalves@outlook.com>
2024-03-12 16:35:08 -0300

    alteracao 8,coloquei um comentario explicando o que a variavel resultado esta somando e subtraindo

--- atividade01_02.js
+++ atividade01_02.js
@@ -5,6 +5,7 @@ let b = 200;
 //criar variavel c
 let c = 10
 //calcule a soma
+//apos calcular a soma calcule a subtracao
 let resultado = a + b - c;
 //exibo o resultado
 //coloco console resultado para aparecer o resultado de uma das variaveis
